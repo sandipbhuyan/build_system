16cd096ca7fb76d7883e4291f14588ec2735e0ed
sandipkumarbhuyan <sandipbhuyan@gmail.com>
2017-08-24 20:47:08 +0530

    [TASK]: Add react bash and modify its styles

--- src/components/404/index.js
+++ src/components/404/index.js
@@ -4,7 +4,7 @@ class FourOhFour extends Component {
     render() {
         return (
             <div>
-                Hello World
+                
             </div>
         )
     }
--- src/components/pagestate/index.js
+++ src/components/pagestate/index.js
@@ -2,36 +2,23 @@ import React, { Component } from 'react'
 import { pageStateUpdate } from '../../actions/pageState'
 import { connect } from 'react-redux'
 import heart from '../../images/heart.png'
+import TerminalBash from '../terminal/index'
 
 class PageState extends Component {
     constructor (props) {
         super(props)
         const { dispatch } = this.props;
         dispatch(pageStateUpdate("heloves"))
-        this.changeState = this.changeState.bind(this)
-        this.changeStateAgain = this.changeStateAgain.bind(this)
     }
 
-    changeState() {
-        const { dispatch } = this.props;
-        dispatch(pageStateUpdate('back'))
-
-    }
-
-    changeStateAgain() {
-        const { dispatch } = this.props
-        dispatch(pageStateUpdate('heloves'))
-    }
     render() {
 
         return(
             <div className='main-page'>
-                <h1 className={this.props.pageStateReducer.pageState === 'heloves' ? '' : 'hide' }>Welcome to my world <span>,Sandip..</span></h1>
-                <h1 className={this.props.pageStateReducer.pageState === 'back' ? '' : 'hide' }><span>Arpita</span></h1>
-                <img className={this.props.pageStateReducer.pageState === 'back' ? '' : 'hide' } src={heart} alt=""/>
-                <div className='increase-page'>
-                    <a className={this.props.pageStateReducer.pageState === 'heloves' ? 'btn btn-red' : 'hide' } href='#' onClick={this.changeState}>He Loves</a>
-                    <a className={this.props.pageStateReducer.pageState === 'back' ? 'btn btn-red' : 'hide' } href='#' onClick={this.changeStateAgain}>back</a>
+                <div className='content-container'>
+                    <h1>Welcome to my world <span>,Sandip..</span></h1>
+                    <p>Terminal for oneline webservers</p>
+                    <TerminalBash />
                 </div>
             </div>
         )
